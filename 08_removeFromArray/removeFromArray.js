86c5ce6887c20443b8436c078296ae6f3df7fb4f
sestion <6144926+sestion@users.noreply.github.com>
2025-11-25 09:24:32 +0800

    feat: add solution to exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    let filtered = arr;
+    for (const arg of args) {
+        filtered = filtered.filter((x) => x !== arg);
+    }
+    return filtered;
 };
 
 // Do not edit below this line
