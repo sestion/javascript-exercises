94996fec8e6e090d1a3bdbe7992eda005cc646db
sestion <6144926+sestion@users.noreply.github.com>
2025-11-25 11:42:24 +0800

    feat: add solution to exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b))
+        return "ERROR";
+    if (a < 0 || b < 0)
+        return "ERROR";
 
+    // sum of arithmetic series
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    let terms = end - start + 1;
+    return (start + end) / 2 * terms;
 };
 
 // Do not edit below this line
