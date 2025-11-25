ba548aeed5d03aaed9955ae3119a31a5264b0e13
sestion <6144926+sestion@users.noreply.github.com>
2025-11-25 12:44:44 +0800

    feat: add solution to exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+    const zeroPoint = 32;
+    const degreeSize = 5 / 9;
+    const result = (f - zeroPoint) * degreeSize;
+    return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+    const zeroPoint = 32;
+    const degreeSize = 9 / 5;
+    const result = c * degreeSize + zeroPoint;
+    return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
