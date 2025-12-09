b2ebb7dd42709e8b1d7dfee41a16d3d59b5e1a65
sestion <6144926+sestion@users.noreply.github.com>
2025-12-09 13:40:58 +0800

    feat: add solution to exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let x = Number.parseInt(num);
+    
+    if (Number.isNaN(x) || x < 0) return "OOPS";
+    if (x === 0) return 0;
+    
+    let curr = 1;
+    let prev = 0;
+    for (let i = 1; i < x; i++) {
+        let temp = curr;
+        curr += prev;
+        prev = temp;
+    }
+    return curr;
 };
 
 // Do not edit below this line
