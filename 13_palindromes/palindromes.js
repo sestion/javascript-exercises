e85b53c7ec41eba0cd06365fbbf415b8335a6550
sestion <6144926+sestion@users.noreply.github.com>
2025-12-09 13:02:49 +0800

    feat: add solution to exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let regex = /[a-z0-9]/;
+    let sanitize = Array.from(str.toLowerCase()).filter((x) => regex.test(x));
 
+    let a = sanitize.join("");
+    let b = [...a].reverse().join("");
+
+    return a === b;
 };
 
 // Do not edit below this line
