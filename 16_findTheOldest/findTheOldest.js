092a5b085c0a810ba1e5a2d9022b42ed6230572f
sestion <6144926+sestion@users.noreply.github.com>
2025-12-09 14:07:20 +0800

    feat: add solution to exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort(compareAge);
+    return people[0];
 };
+
+function compareAge(a, b) {
+    if (!("yearOfDeath" in a)) {
+        a.yearOfDeath = new Date().getFullYear();
+    }
+
+    if (!("yearOfDeath" in b)) {
+        b.yearOfDeath = new Date().getFullYear();
+    }
+
+    return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
