36c4c5f1413006cbbf94c8d61d090085b4023c0b
sestion <6144926+sestion@users.noreply.github.com>
2025-12-09 12:34:31 +0800

    feat: add solution to exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(add, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, cur) => acc * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  for (let i = a; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
